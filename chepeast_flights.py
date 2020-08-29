7ec5bf8981de8cdf0499ae9e874013ae571a7fab
sparya6146 <55103515+sparya6146@users.noreply.github.com>
2020-08-29 12:41:05 +0530

    Update chepeast_flights.py

--- chepeast_flights.py
+++ chepeast_flights.py
@@ -20,7 +20,7 @@ def kCheapestPairs(nums1,nums2, k):
 if __name__=='__main__':
     delhi_to_mumbai = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
     mumbai_to_delhi = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75]
-    CheapestPairs = kCheapestPairs(delhi_to_mumbai,mumbai_to_delhi,10)
+    CheapestPairs = kCheapestPairs(sorted(delhi_to_mumbai),sorted(mumbai_to_delhi),10)
     print(sorted(CheapestPairs,key=lambda x: x[0]+x[1]))
     
     
